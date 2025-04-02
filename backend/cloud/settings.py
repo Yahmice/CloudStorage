4211ebe1b7653ce520df655197169f3f1402574c
Psycho <ilya.efremov.0498@gmail.com>
2025-04-02 19:33:29 +0500

    chagnes

--- backend/cloud/settings.py
+++ backend/cloud/settings.py
@@ -150,7 +150,7 @@ WSGI_APPLICATION = 'cloud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DB_NAME', 'cloudstorage'),
         'USER': os.environ.get('DB_USER', 'postgres'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
