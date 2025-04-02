8b824c9208167497b8d6c838278a955505be4f3d
Psycho <ilya.efremov.0498@gmail.com>
2025-04-02 19:11:04 +0500

    Fixes

--- backend/cloud/settings.py
+++ backend/cloud/settings.py
@@ -143,6 +143,9 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
+        'OPTIONS': {
+            'client_encoding': 'UTF8',
+        },
     }
 }
 
